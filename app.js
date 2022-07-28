1ae8cbb6ac9fdc3a1a2a02337b468239464ee798
anton startsev <anton.startsev@osome.com>
2022-07-28 17:03:00 +0300

    schema

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(PORT);
+});
