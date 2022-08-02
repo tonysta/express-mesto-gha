3d77e9fc50c2a7a5e14860a4bd395e905afc1fe3
anton startsev <anton.startsev@osome.com>
2022-08-02 20:58:53 +0300

    fix

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use((req, res, next) => {
 
 app.use('/', usersRoute);
 app.use('/', cardsRoute);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Cтраницы не существует' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
