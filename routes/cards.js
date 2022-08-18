4da1d734e254c4024b23742de3a87ba07667b249
anton startsev <anton.startsev@osome.com>
2022-08-18 17:25:02 +0300

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -10,7 +10,12 @@ const {
 } = require('../controllers/cards');
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
+cardRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().regex(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/).required(),
+  }),
+}), createCard);
 cardRouter.delete('/:cardId', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
