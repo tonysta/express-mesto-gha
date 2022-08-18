6333c8d49e44b38c5eb3f0295c7b725353ea5979
anton startsev <anton.startsev@osome.com>
2022-08-18 12:48:00 +0300

    fix

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.link(),
+    avatar: Joi.string().regex(/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
