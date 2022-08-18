1880d031d421b1b97744d3c87c85fa319af0d24a
anton startsev <anton.startsev@osome.com>
2022-08-18 12:36:11 +0300

    fix

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.use(bodyParser.urlencoded({
 app.post('/signin', login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.link(),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    avatar: Joi.link(),
-    about: Joi.string().min(2).max(30),
   }),
 }), createUser);
 app.use(auth);
